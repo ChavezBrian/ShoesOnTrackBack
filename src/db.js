ec52b9d75aa0e4c9ac7b7a3dba4f99c0caea8c4a
Brian Chavez <lionelbrian11@gmail.com>
2023-11-28 17:24:51 -0300

    postgres

--- src/db.js
+++ src/db.js
@@ -8,7 +8,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 const sequelize = new Sequelize(
   `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   {
-    dialect: postgresql,
+    dialect: 'postgres',
     logging: false,
     native: false,
   }
