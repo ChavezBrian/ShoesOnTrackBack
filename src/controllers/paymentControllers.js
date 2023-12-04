9f99f4aa11558f608ddfff8d936411ce1694b4ce
Brian Chavez <lionelbrian11@gmail.com>
2023-12-04 12:57:24 -0300

    email

--- src/controllers/paymentControllers.js
+++ src/controllers/paymentControllers.js
@@ -86,7 +86,7 @@ const captureOrder = async (req, res) => {
     if (response.data.status === "COMPLETED") {
       //Enviaremos la notificacion del pago
       const emailResult = await sendEmail(
-        "tucorreo@gmail.com", // Cambia por la dirección de correo a la que deseas enviar la notificación
+        "lionelbrian11@gmail.com", // Cambia por la dirección de correo a la que deseas enviar la notificación
         "Notificación de Pago",
         "Has realizado con éxito la compra del siguiente ticket :"
       );
