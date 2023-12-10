158deb928b91b444a32f1fa7e469b3786289e1e0
Brian Chavez <lionelbrian11@gmail.com>
2023-12-10 16:31:03 -0300

    nuevo deploy

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,9 @@ const productRouter = require("./productsRouter");
 const categoryRouter = require("./categoryRouter");
 const userRouter = require("./userRouter");
 const paymentRouter = require("./paymentRouter");
+const favsRouter = require("./favoritesRoute");
+const cartRouter = require("./cartRouter");
+const sendEmailRouter = require("./sendEmailRouter");
 
 const router = Router();
 
@@ -10,5 +13,8 @@ router.use("/products", productRouter);
 router.use("/categories", categoryRouter);
 router.use("/payment", paymentRouter);
 router.use("/users", userRouter);
+router.use("/favs", favsRouter);
+router.use("/cart", cartRouter);
+router.use("/contact", sendEmailRouter);
 
 module.exports = router;
